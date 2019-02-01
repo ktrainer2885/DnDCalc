31ea7000a937a02de68decafe00a626906e386f8
Rogelio Ramirez <rogelio.ramirez@gmail.com>
2019-02-01 13:42:31 -0600

    Added user input. Issue #10

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,26 @@
 import simulation.Sim;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Sim sim = new Sim(1,3, 2);
+        int partySize;
+        int encounterSize;
+        int simIterations;
+
+
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+
+        System.out.print("Please type in the party size: ");
+        partySize = reader.nextInt();
+        System.out.print("Please type in the encounter size: ");
+        encounterSize = reader.nextInt();
+        System.out.print("Please type in the number of simulations: ");
+        simIterations = reader.nextInt();
+
+        Sim sim = new Sim(partySize,encounterSize, simIterations);
         sim.simulation();
     }
 }
