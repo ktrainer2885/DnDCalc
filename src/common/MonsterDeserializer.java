62346aedf54ced3d5f450b92055a1cec3b33b276
Rogelio Ramirez <rogelio.ramirez@gmail.com>
2019-03-22 12:34:21 -0500

    Implemented basic reading of JSON Monster file

--- src/common/MonsterDeserializer.java
+++ src/common/MonsterDeserializer.java
@@ -27,6 +27,25 @@ public class MonsterDeserializer extends StdDeserializer<Monster> {
         Monster monster;
         String name;
         Integer ac;
+        Integer hpDiceNum;
+        Integer hpDiceType;
+        Integer hpConstant = 0;
+        Integer prof = 0;
+        Integer str;
+        Integer dex;
+        Integer con;
+        Integer intel;
+        Integer wis;
+        Integer cha;
+        String weapon = "0d0";
+        String[] damDice;
+        Integer damCont = 0;
+
+        String hpFormula;
+        String[] hpInfo; // 0  - num dice , 1 - dice type, 2 - hp constant
+        String profString;
+        String[] profStrings;
+
 
         JsonNode jsonNode = jsonParser.getCodec().readTree(jsonParser);
 
@@ -39,10 +58,71 @@ public class MonsterDeserializer extends StdDeserializer<Monster> {
             ac = (Integer) jsonNode.get("ac").get(0).get("ac").numberValue();
         }
 
+        // sets hp for monsters
+        hpFormula = (String) jsonNode.get("hp").get("formula").asText();
+        hpInfo = hpFormula.split("d|\\+|\\s");
+        hpDiceNum = Integer.parseInt(hpInfo[0]);
+        hpDiceType = Integer.parseInt(hpInfo[1]);
+        if (hpInfo.length == 3){
+            hpConstant = Integer.parseInt(hpInfo[2]);
+        }
+
+        // sets proficiency
+        profString = (String) jsonNode.get("action").get(0).get("name").asText();
+        if (profString.equalsIgnoreCase("multiattack")){
+            profString = (String) jsonNode.get("action").get(1).get("entries").get(0).asText();
+        }
+        else {
+            profString = (String) jsonNode.get("action").get(0).get("entries").get(0).asText();
+        }
+
+        //System.out.println(profString);
+        profStrings = profString.split("@hit|@damage");
+
+        for (int j = 0; j < profStrings.length; j++) {
+            int i;
+            i = profStrings[j].indexOf("}");
+            if (i > -1){
+                profStrings[j] = profStrings[j].substring(0,i);
+                profStrings[j] = profStrings[j].trim();
+
+                // adds in proficiency
+                if (profStrings[j].matches("-?\\d+(\\.\\d+)?")){
+                    prof = Integer.parseInt(profStrings[j]);
+                }
+
+                // adds in damage dice
+
+                if(profStrings[j].matches("-?\\d+d\\d+\\s*(\\+\\s*\\d+)?")){
+                    damDice = profStrings[j].split("\\+");
+                    for (int k = 0; k < damDice.length; k++){
+                        damDice[k] = damDice[k].trim();
+                    }
+
+                    weapon = damDice[0];
+                    if(damDice.length > 1 ){
+                        damCont = Integer.parseInt(damDice[1]);
+                    }
+                }
+
+            }
+            //System.out.println(profStrings[j]);
+        }
+
+        // Add in attributes
+
+        str = (Integer) jsonNode.get("str").numberValue();
+        dex = (Integer) jsonNode.get("dex").numberValue();
+        con = (Integer) jsonNode.get("con").numberValue();
+        intel = (Integer) jsonNode.get("int").numberValue();
+        wis = (Integer) jsonNode.get("wis").numberValue();
+        cha = (Integer) jsonNode.get("cha").numberValue();
 
 
-        monster = new Monster(name,ac,10,10,10,10,
-                10,10,10,10,10,10,"2d8",10);
+        //prof = Integer.parseInt(profStrings[1]);
+        monster = new Monster(name,ac,hpDiceNum,hpDiceType,hpConstant,prof,
+                str,dex,con,intel,wis,cha,weapon,damCont);
         return monster;
     }
+
 }
--- src/simulation/JsonTest.java
+++ src/simulation/JsonTest.java
@@ -21,6 +21,9 @@ public class JsonTest {
             monster = mapper.readValue(file, Monster.class);
             System.out.println(monster.getName());
             System.out.println(monster.getAc());
+            System.out.println(monster.getHp());
+            System.out.println(monster.getProf());
+            System.out.println(monster.attackDamage());
 
             File files = new File("files/monsters/jsonBigTest.json");
             System.out.println(files.getCanonicalPath());
@@ -28,8 +31,18 @@ public class JsonTest {
 
             for ( Monster m: monsters
                  ) {
-                System.out.println(m.getName());
+                System.out.println();
+                System.out.println("Name: " + m.getName());
                 System.out.println("AC: " + m.getAc());
+                System.out.println("HP: " + m.getHp());
+                System.out.println("Prof: " + m.getProf());
+                System.out.println("Damage Dealt: " +m.attackDamage());
+                System.out.println("STR: " + m.getStr());
+                System.out.println("DEX: " + m.getDex());
+                System.out.println("CON: " + m.getCon());
+                System.out.println("INT: " + m.getIntel());
+                System.out.println("WIS: " + m.getWis());
+                System.out.println("CHA: " + m.getCha());
             }
 
 
