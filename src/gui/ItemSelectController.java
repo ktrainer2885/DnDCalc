91344fbaaa016607c1b9c6ad0f762f3ea7b468cf
Oscar-3 <oalvareziii0315@gmail.com>
2019-04-01 18:18:32 -0500

    Another GUI update!  Attributes all set for smooth integration into Item Class.

--- src/gui/ItemSelectController.java
+++ src/gui/ItemSelectController.java
@@ -1,135 +1,162 @@
 package gui;
 
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import common.Commands;
-import common.Creature;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.application.Application;
-import java.lang.Object;
-import javafx.scene.Scene;
-import javafx.scene.Node;
-import javafx.stage.Stage;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
-import monster.Monster;
-import player.*;
-import simulation.Sim;
 import javafx.scene.Parent;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
+import javafx.scene.layout.Region;
 
 public class ItemSelectController {
 
-    ObservableList<String> itemList = FXCollections.observableArrayList("Sword", "Mace", "Chain mail", "Shield");
+    //Array for weapon ChoiceBox is initialized
+    ObservableList<String> weaponList = FXCollections.observableArrayList("Sword", "Mace", "Lance", "Bow");
+    //Array for armor ChoiceBox is initialized
+    ObservableList<String> armorList = FXCollections.observableArrayList("Cloth", "Leather", "Chain Mail", "Scale Mail");
+    //Array for ammo ChoiceBox is initialized
+    ObservableList<String> ammoList = FXCollections.observableArrayList("Arrows", "Bolts", "Bullets", "Rocks");
 
+    // Initializes the items into the respective choiceBox
     @FXML
     private void initialize(){
-        itemBox.setItems(itemList);
-        item1Box.setItems(itemList);
+        weaponBox.setItems(weaponList);
+        armorBox.setItems(armorList);
+        ammoBox.setItems(ammoList);
     }
 
     @FXML
-    private ChoiceBox itemBox;
+    private ChoiceBox weaponBox;
     @FXML
-    private TextField magicMod;
+    private TextField weapMagicMod;
 
     @FXML
-    private ChoiceBox item1Box;
+    private ChoiceBox armorBox;
     @FXML
-    private TextField magic1Mod;
+    private TextField armMagicMod;
+
+    @FXML
+    private ChoiceBox ammoBox;
+    @FXML
+    private TextField ammoCount;
 
     @FXML
     void submitButtonClicked(){
-
+        
         try{
+            //methods to communicate with HomoController class
             FXMLLoader load = new FXMLLoader(getClass().getResource("Home.fxml"));
             Parent root = (Parent) load.load();
             HomeController home = load.getController();
 
         if(home.item == 0) {
-            if (itemBox.getValue() != null) {
-                home.setItemA(itemBox.getValue().toString());
+            if (weaponBox.getValue() != null) {
+                home.setItemA(weaponBox.getValue().toString());
             }
-            if (item1Box.getValue() != null){
-                home.setItemB(item1Box.getValue().toString());
+            if (weapMagicMod.getText() != null){
+                home.setTextA(weapMagicMod.getText());
             }
-            if (magicMod.getText() != null){
-                home.setTextA(magicMod.getText());
+            if (armorBox.getValue() != null){
+                home.setItemB(armorBox.getValue().toString());
             }
-            if (magic1Mod.getText() != null){
-                home.setTextB(magic1Mod.getText());
+            if (armMagicMod.getText() != null){
+                home.setTextB(armMagicMod.getText());
             }
+            if (ammoBox.getValue() != null){
+                home.setItemC(ammoBox.getValue().toString());
+            }
+            if (ammoCount.getText() != null){
+                home.setTextC(ammoCount.getText());
+            }
+
             Alert alert = new Alert(AlertType.INFORMATION);
             alert.setTitle("Items");
             alert.setHeaderText(null);
-            alert.setContentText("\nItem A: " + home.playerItemA + "  Magic A: " + home.playerTextA + "  Item B: " + home.playerItemB + "  Magic B: " + home.playerTextB);
+            alert.setContentText("\nWeapon: " + home.playerWeap + "  Mag Mod: " + home.playerWeapMagMod + "  Armor: " + home.playerArm + "  Mag Mod: " + home.playerArmMagMod + "  Ammo: " + home.playerAmmo + "  Count: " + home.playerAmmoCount);
+            alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
             alert.showAndWait();
         }
         if(home.item == 1) {
-            if (itemBox.getValue() != null) {
-                home.setItem1A(itemBox.getValue().toString());
+            if (weaponBox.getValue() != null) {
+                home.setItem1A(weaponBox.getValue().toString());
             }
-            if (item1Box.getValue() != null){
-                home.setItem1B(item1Box.getValue().toString());
+            if (weapMagicMod.getText() != null){
+                home.setText1A(weapMagicMod.getText());
             }
-            if (magicMod.getText() != null){
-                home.setText1A(magicMod.getText());
+            if (armorBox.getValue() != null){
+                home.setItem1B(armorBox.getValue().toString());
             }
-            if (magic1Mod.getText() != null){
-                home.setText1B(magic1Mod.getText());
+            if (armMagicMod.getText() != null){
+                home.setText1B(armMagicMod.getText());
+            }
+            if (ammoBox.getValue() != null){
+                home.setItem1C(ammoBox.getValue().toString());
+            }
+            if (ammoCount.getText() != null){
+                home.setText1C(ammoCount.getText());
             }
             Alert alert = new Alert(AlertType.INFORMATION);
             alert.setTitle("Items 1");
             alert.setHeaderText(null);
-            alert.setContentText("\nItem 1A: " + home.player1ItemA + "  Magic 1A: " + home.player1TextA + "  Item 1B: " + home.player1ItemB + "  Magic 1B: " + home.player1TextB);
+            alert.setContentText("\nWeapon1: " + home.player1Weap + "  Mag Mod1: " + home.player1WeapMagMod + "  Armor1: " + home.player1Arm + "  Mag Mod1: " + home.player1ArmMagMod + "  Ammo1: " + home.player1Ammo + "  Count1: " + home.player1AmmoCount);
+            alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
             alert.showAndWait();
         }
         if(home.item == 2) {
-            if (itemBox.getValue() != null) {
-                home.setItem2A(itemBox.getValue().toString());
+            if (weaponBox.getValue() != null) {
+                home.setItem2A(weaponBox.getValue().toString());
             }
-            if (item1Box.getValue() != null){
-                home.setItem2B(item1Box.getValue().toString());
+            if (weapMagicMod.getText() != null){
+                home.setText2A(weapMagicMod.getText());
             }
-            if (magicMod.getText() != null){
-                home.setText2A(magicMod.getText());
+            if (armorBox.getValue() != null){
+                home.setItem2B(armorBox.getValue().toString());
             }
-            if (magic1Mod.getText() != null){
-                home.setText2B(magic1Mod.getText());
+            if (armMagicMod.getText() != null){
+                home.setText2B(armMagicMod.getText());
+            }
+            if (ammoBox.getValue() != null){
+                home.setItem2C(ammoBox.getValue().toString());
+            }
+            if (ammoCount.getText() != null){
+                home.setText2C(ammoCount.getText());
             }
             Alert alert = new Alert(AlertType.INFORMATION);
             alert.setTitle("Items 2");
             alert.setHeaderText(null);
-            alert.setContentText("\nItem 2A: " + home.player2ItemA + "  Magic 2A: " + home.player2TextA + "  Item 2B: " + home.player2ItemB + "  Magic 2B: " + home.player2TextB);
+            alert.setContentText("\nWeapon2: " + home.player2Weap + "  Mag Mod2: " + home.player2WeapMagMod + "  Armor2: " + home.player2Arm + "  Mag Mod2: " + home.player2ArmMagMod + "  Ammo2: " + home.player2Ammo + "  Count2: " + home.player2AmmoCount);
+            alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
             alert.showAndWait();
         }
         if(home.item == 3) {
-            if (itemBox.getValue() != null) {
-                home.setItem3A(itemBox.getValue().toString());
+            if (weaponBox.getValue() != null) {
+                home.setItem3A(weaponBox.getValue().toString());
             }
-            if (item1Box.getValue() != null){
-                home.setItem3B(item1Box.getValue().toString());
+            if (weapMagicMod.getText() != null){
+                home.setText3A(weapMagicMod.getText());
             }
-            if (magicMod.getText() != null){
-                home.setText3A(magicMod.getText());
+            if (armorBox.getValue() != null){
+                home.setItem3B(armorBox.getValue().toString());
             }
-            if (magic1Mod.getText() != null){
-                home.setText3B(magic1Mod.getText());
+            if (armMagicMod.getText() != null){
+                home.setText3B(armMagicMod.getText());
+            }
+            if (ammoBox.getValue() != null){
+                home.setItem3C(ammoBox.getValue().toString());
+            }
+            if (ammoCount.getText() != null){
+                home.setText3C(ammoCount.getText());
             }
 
             Alert alert = new Alert(AlertType.INFORMATION);
             alert.setTitle("Items 3");
             alert.setHeaderText(null);
-            alert.setContentText("\nItem 3A: " + home.player3ItemA + "  Magic 3A: " + home.player3TextA + "  Item 3B: " + home.player3ItemB + "  Magic 3B: " + home.player3TextB);
+            alert.setContentText("\nWeapon3: " + home.player3Weap + "  Mag Mod3: " + home.player3WeapMagMod + "  Armor3: " + home.player3Arm + "  Mag Mod3: " + home.player3ArmMagMod + "  Ammo3: " + home.player3Ammo + "  Count3: " + home.player3AmmoCount);
+            alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
             alert.showAndWait();
         }
 
